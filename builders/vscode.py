9d8013bc20511160aea092a03aab459570a6c537
James Barzegar <james.barzegar@gmail.com>
2021-11-21 11:17:57 -0500

    wip: added vscode builder
    vscode is using snap to do installs

---
+++ builders/vscode.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import subprocess
+from getpass import getpass
+
+
+def build():
+    print('installing vscode via snap!')
+
+    pw = getpass('Please provided sudo password: ')
+
+    proc = subprocess.Popen(
+        ['sudo -S /usr/bin/snap install code --classic'],
+        shell=True,
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    try:
+        outs, errs = proc.communicate(pw.encode(), timeout=15)
+
+        err_str = str(errs)
+
+        if "is already installed" in err_str:
+            print(err_str)
+        else:
+            raise Exception(err_str)
+
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        raise Exception("build timed out")
+    except Exception as err:
+        raise err
