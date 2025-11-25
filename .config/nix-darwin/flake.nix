6f74b60169b46a39847710b350fc0f3f9616af94
James Barzegar <james.barzegar@gmail.com>
2025-11-24 22:18:03 -0500

    nix config

---
+++ .config/nix-darwin/flake.nix
@@ -0,0 +1,56 @@
+# nix-darwin docs: https://mynixos.com/nix-darwin
+
+{
+  description = "🦝";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      nix-darwin,
+      home-manager,
+      ...
+    }:
+    let
+      configuration =
+        { ... }:
+        {
+          system.primaryUser = "james";
+          # environment.systemPackages = [ pkgs.vim ];
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # set commit has for darwin-version
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          nixpkgs.config.allowUnfree = true;
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Jamess-MacBook-Pro
+      darwinConfigurations."Jamess-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          home-manager.darwinModules.home-manager
+          configuration
+          ./config.nix
+          ./darwin.nix
+          ./aerospace.nix # Setup windowmanager
+        ];
+      };
+    };
+}
