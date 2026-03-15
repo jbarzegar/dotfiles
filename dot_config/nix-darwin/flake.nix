527701610ab0e513058570599a2e35d6dd38b270
James Barzegar <james.barzegar@gmail.com>
2026-03-15 19:25:29 -0400

    change hostname

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -43,7 +43,7 @@
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#Jamess-MacBook-Pro
-      darwinConfigurations."Jamess-MacBook-Pro-2.local" = nix-darwin.lib.darwinSystem {
+      darwinConfigurations."Jamess-MacBook-Pro-2" = nix-darwin.lib.darwinSystem {
         modules = [
           home-manager.darwinModules.home-manager
           configuration
